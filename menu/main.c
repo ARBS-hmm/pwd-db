d52f8ad3520c73dc15f83a5c094d22199551d104
ARBS-hmm <aayushreg@gmail.com>
2025-03-06 01:38:40 +0545

    added options to the menu sections. PENDING functions.....

--- menu/main.c
+++ menu/main.c
@@ -7,7 +7,7 @@ void startSession(char name[]){
   FILE *fp;
   char path[30];
   char extension[10];
-  char prompt[10];
+  char cmd[10], args[10];
   char key[10];
 
   strcpy(extension, ".txt");
@@ -25,10 +25,28 @@ void startSession(char name[]){
   printf("Welcome %s\n", name);
   while(1){
     printf("%s> ",name);
-    scanf("%s", key);
     //REPL here ig??
     fclose(fp);
-    query(key,path); //test
+    scanf("%s %s", cmd, args);
+    if (strcmp(cmd,"check")==0){
+    //  printf("hmm\n");
+      query(args,path); //test
+    }
+    else if (strcmp(cmd,"update")==0){
+      printf("Update PENDING\n");
+      fopen(path,"r");
+  // 	update(key,path);
+      // 	check if it exists ig? and then overwrite it.
+      //syslog(name, key, pwd, "user has updated their password");
+    }
+    else if (strcmp(cmd,"del")==0){
+      printf("Delete PENDING\n");
+      fopen(path,"r");
+    }
+    else{
+      printf("invalid option try something else...\n");
+      fopen(path,"r");
+    }
   }
 }
 
@@ -62,7 +80,7 @@ void query(char searchKey[], char path[]){
   char key[10];
   char pwd[10];
 
-  printf("path = %s\n", path);
+  //printf("path = %s\n", path);
   fp = fopen(path, "r");
   if (fp == NULL){
     printf("Error\n");
@@ -74,9 +92,9 @@ void query(char searchKey[], char path[]){
   while(fscanf(fp,"%s %s\n",key,pwd)==2){
     //printf("key = %s\n", key);
     if (strcmp(key,searchKey)==0){
-      printf("Found\n");
+      printf("password: %s\n", pwd);
       i++;
-      printf("line number = %d\n", i);
+      //printf("line number = %d\n", i);
       i--;
       return;
     };
