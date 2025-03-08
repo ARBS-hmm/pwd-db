ccbed7c95cf63330fbfb51c32549263a770b5327
ARBS-hmm <aayushreg@gmail.com>
2025-03-08 14:23:21 +0545

    scripting is a headache

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/run/current-system/sw/bin/zsh
 
-
 # Set paths for directories
 ROOT_DIR=$(pwd)  # Current directory (root of the project)
 MODULES_DIR="${ROOT_DIR}/modules-ig"
@@ -9,6 +8,10 @@ LIB_DIR="${ROOT_DIR}/lib"
 
 # Create lib directory if it doesn't exist
 mkdir -p "$LIB_DIR"
+
+cp $ROOT_DIR/lib/io.so $ROOT_DIR/modules-ig/fileHandlers/
+cp $ROOT_DIR/lib/user.so $ROOT_DIR/modules-ig/fileHandlers/
+echo "Copied the dumb so files"
 
 # Compile all .c files in the modules-ig directory into .so files in the lib directory
 echo "Compiling .c files in ${MODULES_DIR} into .so files..."
@@ -26,10 +29,6 @@ if [ -z "$LIBRARY_FILES" ]; then
   echo "No shared libraries found in $LIB_DIR. Exiting."
   exit 1
 fi
-
-cp $ROOT_DIR/lib/io.so $ROOT_DIR/modules-ig/fileHandlers/
-cp $ROOT_DIR/lib/user.so $ROOT_DIR/modules-ig/fileHandlers/
-echo "Copied the dumb so files"
 
 # Explicitly list the libraries to link (example: -l module1 -l module2)
 # Modify this list based on the actual libraries you have compiled
