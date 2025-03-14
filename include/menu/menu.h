77ea77eca43fd9459c3292b70df09315954ed349
ARBS-hmm <aayushreg@gmail.com>
2025-03-14 13:05:44 +0545

    idek

---
+++ include/menu/menu.h
@@ -0,0 +1,12 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void mainMenu();
+
+void loginScreen(char name[]);
+
+#endif 
---
+++ modules-ig/menu/menu.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+//ig at the end?
+
+void mainMenu(){
+
+  return;
+}
+
+void loginScreen(char username[]){
+
+  return;
+}
